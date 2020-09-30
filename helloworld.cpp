d1580aee55dff466ea1c1386809dae88e11b099c
Saumya Chitransh <saumyachitransh3001@gmail.com>
2020-09-30 21:50:27 +0530

    Semi colon added

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
-int main() {
-   printf("Hello, World!");
-   printf("saumya gawar")
+int main()
+{
+   printf("Hello, World!\n");
+   //Semi colon added and new line
+   printf("Tu gawar\n");
    return 0;
 }
